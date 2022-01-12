8aa553891c1b113b0ca4ce740cfd00edf61870d8
memesparky <memesparky@users.noreply.github.com>
2022-01-12 13:35:43 -0500

    Basic Functionality for click Chords enabled

--- guitar_keys/lib/main.dart
+++ guitar_keys/lib/main.dart
@@ -47,69 +47,100 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+//Column Selector Array
+Map NoteKeys = {
+  "A♭": ["Yo", "Hi"],
+  "A": ["Yo", "Hi"],
+  "B♭": ["Yo", "Hi"],
+  "B": ["Yo", "Hi"],
+  "C": ["Yo", "Hi"],
+  "C♯": ["Yo", "Hi"],
+  "D♭": ["Yo", "Hi"],
+  "D": ["Yo", "Hi"],
+  "E♭": ["Yo", "Hi"],
+  "E": ["Yo", "Hi"],
+  "F": ["Yo", "Hi"],
+  "F♯": ["Yo", "Hi"],
+  "G♭": ["Yo", "Hi"],
+  "G": ["Yo", "Hi"],
+};
+var Notes = {
+  "A♭",
+  "A",
+  "B♭",
+  "B",
+  "C",
+  "C♯",
+  "D♭",
+  "D",
+  "E♭",
+  "E",
+  "F",
+  "F♯",
+  "G♭",
+  "G"
+};
+String currentNote = Notes.elementAt(0).toString();
+List currentList = NoteKeys[currentNote];
+List NoteNumber = ["I", "ii", "iii", "IV", "V", "vi", "vii"];
+
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width / 3,
+              child: ListView.builder(
+                  itemCount: Notes.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return TextButton(
+                      child: Text(
+                        Notes.elementAt(index).toString(),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          currentNote = Notes.elementAt(index).toString();
+                          currentList = NoteKeys[currentNote];
+                        });
+                      },
+                    );
+                  }),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width / 3,
+              child: ListView.builder(
+                  itemCount: currentList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                        child: Container(
+                      child: Text(currentList[index]),
+                    ));
+                  }),
             ),
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width / 3,
+              child: ListView.builder(
+                  itemCount: NoteNumber.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                        child: Container(
+                      child: Text(NoteNumber[index].toString()),
+                    ));
+                  }),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
